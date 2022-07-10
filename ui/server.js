fdcdea0e5ac8a867868232777fcdfa669ea2b2c6
zakken <matheus@marcogabriel.com.br>
2022-07-09 22:14:56 -0300

    Binding requests

--- ui/server.js
+++ ui/server.js
@@ -3,12 +3,28 @@ require("../dotenv");
 const http = require("http");
 const routes = require("./routes");
 const urlModule = require('url');
-const queryString = require('querystring')
+const queryString = require('querystring');
+const Logger = require('../shared/logger'); 
+
+const logger = new Logger();
 
 const server = http.createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST");
     res.setHeader("Access-Control-Allow-Headers", "*");
+
+    const ip = req.connection.remoteAddress;
+    logger.info(`${ip} - Start request: ${req.method}: ${req.url}`)
+
+    const started = new Date();
+    res.on('finish', () => {
+        const took = new Date() - started;
+
+        logger.info(
+            `${ip} - Request done: ${req.method}: ` +
+            `${req.url} ${res.statusCode} ${took}ms`
+        );
+    });
 
     if (req.method === "OPTIONS") { return res.end();}
 
@@ -32,6 +48,6 @@ server.listen(
         port: process.env.HTTP_PORT_SERVER,
     },
     () => {
-        console.log(`Server is running in ${process.env.HTTP_PORT_SERVER}`);
+        logger.info(`Server is running in ${process.env.HTTP_PORT_SERVER}`);
     }
 );
